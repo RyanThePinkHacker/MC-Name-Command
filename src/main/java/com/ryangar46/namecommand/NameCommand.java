81ae386453c3f9684cb54cce10c7a48731d6cf78
Ryan Garrett <RyanGar46@gmail.com>
2021-08-17 17:50:13 -0400

    Fix parity between forge and fabric
    The forge is slightly different than the fabric version.

--- src/main/java/com/ryangar46/namecommand/NameCommand.java
+++ src/main/java/com/ryangar46/namecommand/NameCommand.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.EntityArgument;
@@ -30,30 +31,35 @@ import java.util.List;
 
 public class NameCommand {
     public static void register(CommandDispatcher<CommandSource> dispatcher) {
-        LiteralArgumentBuilder<CommandSource> nameCommand
-                = Commands.literal("name")
+        dispatcher.register(Commands.literal("name")
                 .requires((commandSource) -> commandSource.hasPermission(2))
                 .then(Commands.argument("targets", EntityArgument.entities())
-                    .then(Commands.argument("name", MessageArgument.message())
-                        .executes((p_198493_0_) -> {
-                            return nameItem(p_198493_0_.getSource(), EntityArgument.getEntities(p_198493_0_, "targets"), MessageArgument.getMessage(p_198493_0_, "name").getString());
-                        })
-                    )
-                );
-        dispatcher.register(nameCommand);
+                        .then(Commands.argument("name", MessageArgument.message())
+                                .executes((context) -> {
+                                    return nameItem(context.getSource(), EntityArgument.getEntities(context, "targets"), MessageArgument.getMessage(context, "name").getString()); }))));
     }
 
     private static int nameItem(CommandSource source, Collection<? extends Entity> target, String name) throws CommandSyntaxException {
         int i = 0;
 
-        for(Entity entity : target) {
+        for (Entity entity : target) {
+            // Checks if they are a Living Entity
             if (entity instanceof LivingEntity) {
                 LivingEntity livingentity = (LivingEntity)entity;
                 ItemStack itemstack = livingentity.getMainHandItem();
+                // Checks if slot is empty
                 if (!itemstack.isEmpty()) {
+                    i++;
                     itemstack.setHoverName(ITextComponent.nullToEmpty(name));
                 }
             }
+        }
+
+        if (i > 0) {
+            source.sendSuccess(new TranslationTextComponent("command.name.success", i, name), true);
+        }
+        else {
+            throw new SimpleCommandExceptionType(new TranslationTextComponent("command.name.fail")).create();
         }
 
         return i;
